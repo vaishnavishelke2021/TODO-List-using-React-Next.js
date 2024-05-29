fdef469c0c15e338cf37072d8d33d4101f1fa63d
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-05-29 21:55:35 +0530

    form added

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[#161616] justify-center align-middle">
       <body className={inter.className}>{children}</body>
     </html>
   );
