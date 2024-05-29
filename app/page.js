eb67fa892b21a2adbc22de0375bf80c23970d13f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-05-29 23:38:05 +0530

    delete and complete button added

--- app/page.js
+++ app/page.js
@@ -7,9 +7,21 @@ const page = () => {
 
   const submitHandler = (e) => {
     e.preventDefault();
-    setMainTask([...mainTask, { task }]);
+    setMainTask([...mainTask, { task, completed: false }]);
     console.log(mainTask);
     setTask("");
+  };
+
+  const deleteHandler = (i) => {
+    const copyTasks = [...mainTask];
+    copyTasks.splice(i, 1);
+    setMainTask(copyTasks);
+  };
+
+  const completeHandler = (i) => {
+    const copyTasks = [...mainTask];
+    copyTasks[i].completed = !copyTasks[i].completed;
+    setMainTask(copyTasks);
   };
 
   let renderTask = (
@@ -19,18 +31,25 @@ const page = () => {
   );
 
   if (mainTask.length > 0) {
-    renderTask = mainTask.map((t) => {
+    renderTask = mainTask.map((t, i) => {
       return (
-        <div className=" w-full bg-[#201f1f92] py-1 mt-2 flex align-middle items-center justify-between px-3 border-b-[1px] border-[#f9f9f916]">
+        <div
+          key={i}
+          className=" w-full bg-[#201f1f92] py-1 mt-2 flex align-middle items-center justify-between px-3 border-b-[1px] border-[#f9f9f916]"
+        >
           <h1
-            key={task}
-            className=" text-[1rem] text-[#f9f9f9e7]  text-center rounded-sm"
+            className={`text-[1rem] text-[#f9f9f9e7] text-center rounded-sm ${
+              t.completed ? "line-through" : ""
+            }`}
           >
             {t.task}
           </h1>
 
           <div className="flex space-x-4">
-            <button className=" text-green-500 py-1">
+            <button
+              className=" text-green-400 py-1"
+              onClick={() => completeHandler(i)}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -42,7 +61,10 @@ const page = () => {
                 <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z" />
               </svg>
             </button>
-            <button className=" text-red-500 py-1">
+            <button
+              className=" text-red-400 py-1"
+              onClick={() => deleteHandler(i)}
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -80,7 +102,7 @@ const page = () => {
           </button>
         </form>
 
-        <ul className="mt-5">
+        <ul className="mt-7">
           <li>{renderTask}</li>
         </ul>
       </div>
